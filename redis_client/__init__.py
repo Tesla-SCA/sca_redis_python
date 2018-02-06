e31644d63eaf14f1e1b6e03538279241e36a899c
Sona Allahverdiyeva <sonakhan@stanford.edu>
2018-02-05 16:25:14 -0800

    fixed naming + added logging

--- redis_client/__init__.py
+++ redis_client/__init__.py
@@ -19,15 +19,16 @@ class RedisClient:
     def __generate_md5_redis_key(event, context):
         event_str = str(event)
         lambda_arn = context.invoked_function_arn
-        str_to_md5 = event_str + lambda_arn
-        return hashlib.md5(str_to_md5.encode('utf-8')).hexdigest()
+        str_for_md5 = event_str + lambda_arn
+        return hashlib.md5(str_for_md5.encode('utf-8')).hexdigest()
 
     @staticmethod
     def __generic_generate_md5_redis_key(function_name, payload):
         function_str = str(function_name)
         payload_str = str(payload)
-        str_to_md5 = function_str + payload_str
-        return hashlib.md5(str_to_md5.encode('utf-8')).hexdigest()
+        print("Generating md5_redis_key for " + function_str)
+        str_for_md5 = function_str + payload_str
+        return hashlib.md5(str_for_md5.encode('utf-8')).hexdigest()
 
     # AWS Lambda Requests handler can be used to interface with any Lambda handlers.
     # This internally invokes RedisClient.fetch_data that has the workflow to cache
@@ -74,13 +75,13 @@ class RedisClient:
         redis_key_md5 = _self.__generic_generate_md5_redis_key(function_name, payload)
 
         _params = cache_params or _self.redis_params()
-
+        print("Cache params set to " + str(_params))
         if _params is not None and (not isinstance(_params, dict) or _self.__ttl_key not in _params):
             raise TypeError("Params must be a dictionary and must provide a TTL value")
 
         method_params = {'payload': payload}
         _params.update({'method_params': method_params})
-
+        print("Passing " + str(_params) + "into fetch_data")
         return _self.fetch_data(redis_key_md5, callback_method, _params)
 
     # Redis client instance. Only create if one doesn't exist.
@@ -123,8 +124,7 @@ class RedisClient:
                 return json.loads(redis_get_result)
             else:
                 # cache miss
-                print(str(_method_attrs))
-                print(str(query_method))
+
                 invoker_method_result = query_method(*_method_attrs[0:2]) if _method_attrs is not None else query_method()
                 _redis_client.set(key, json.dumps(invoker_method_result), int(_params.get(_self.__ttl_key)))
                 print(
